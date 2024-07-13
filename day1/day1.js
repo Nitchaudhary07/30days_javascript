bb7e752f0ea263d5905237ce1713195d969d6966
Nitchaudhary07 <niteshchaudhary491@gmail.com>
2024-07-13 23:53:45 +0530

    Day one is done

---
+++ day1/day1.js
@@ -0,0 +1,73 @@
+// Task 1
+
+var num = 7;
+
+console.log(num);
+
+
+
+// Task 2
+
+let str = "Chet";
+
+console.log(str);
+
+// Task 3
+
+const boolean = true;
+
+console.log(boolean);
+
+
+// Task 4......
+
+// Number
+let age = 25;
+console.log(typeof age); 
+
+// String
+let string = "Alice";
+console.log(typeof string); 
+
+// Boolean
+let booleans = true;
+console.log(typeof booleans); 
+
+// Object
+let obj = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30
+};
+console.log(typeof obj); 
+
+// Array
+let array = ["apple", "banana", "cherry"];
+console.log(typeof array); 
+
+// Undefined
+let undefined_var;
+console.log(typeof undefined_var); 
+
+// Null
+let null_var = null;
+console.log(typeof null_var); 
+
+// Function
+let greet = function() {
+    return "Hello, world!";
+};
+console.log(typeof greet); 
+
+// Symbol
+let symbol = Symbol("symbol");
+console.log(typeof symbol); 
+
+
+// Task 5
+
+let myAge = 18;
+
+console.log(myAge);
+age = 20;
+console.log("New Age :",myAge);
